c050a56fba63692ca236f1a6a13da146c8dd428c
Gustavo Morais <gmos.99@gmail.com>
2025-04-22 09:40:54 -0300

    Atividade 2

---
+++ src/E2.java
@@ -0,0 +1,25 @@
+public class E2 {
+    /*
+    * Desenvolva um programa que solicite números ao usuário e some-os enquanto os números fornecidos forem positivos.
+        Requisitos:
+        - Utilizar um while que continue a iterar enquanto o número informado for positivo;
+        - Ao receber um número negativo, encerrar o loop e exibir o total acumulado dos números positivos.
+    * */
+
+    public static void main(String[] args) {
+        // Como a classe Scanner foi declarada no momento de criar o objeto, não é necessário importar a classe Scanner de forma explicita.
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        int soma = 0;
+        int numero;
+
+        do {
+            System.out.print("Digite um número positivo (ou negativo para sair): ");
+            numero = scanner.nextInt();
+            if (numero >= 0) {
+                soma += numero;
+            }
+        } while (numero >= 0);
+
+        System.out.println("Soma dos números positivos: " + soma);
+    }
+}
