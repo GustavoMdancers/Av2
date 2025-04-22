2349d1ac1bd238b63ccc5914a92be4074d4650aa
Gustavo Morais <gmos.99@gmail.com>
2025-04-22 08:06:45 -0300

    Atividade 1

---
+++ src/E1.java
@@ -0,0 +1,20 @@
+public class E1 {
+    /*
+    * Crie um programa que utilize um loop while para imprimir, em linhas separadas, os números de 1 até 50.
+        Requisitos:
+
+        - Utilizar a estrutura de repetição while;
+        - Imprimir cada número em uma nova linha. Crie um programa que utilize um loop while para imprimir, em linhas separadas, os números de 1 até 50.
+        Requisitos:
+        - Utilizar a estrutura de repetição while;
+        - Imprimir cada número em uma nova linha.
+    * */
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 50) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
